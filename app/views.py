5d513d15bbc18659af01557fe09c8d32f1f85f7b
Vaibhav-Lodha <vaibhav.lodha@squadstack.com>
2024-06-26 16:21:02 +0530

    adding examples for anonymizer

--- app/views.py
+++ app/views.py
@@ -1,21 +1,19 @@
 from django.db import connection
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from presidio_analyzer import AnalyzerEngine, RecognizerResult
+from presidio_anonymizer import AnonymizerEngine, OperatorConfig
+
+# drf get view
+from rest_framework import serializers, viewsets
 from rest_framework.response import Response
 
 from client_app.models import Employee
 
+from .models import Client
+
 
 def index(request):
     return HttpResponse("<h1>Public Index</h1>")
-
-
-# drf get view
-
-from rest_framework import viewsets, serializers
-
-from .models import Client
 
 
 class ClientSerializer(serializers.ModelSerializer):
@@ -30,9 +28,60 @@ class ClientViewSet(viewsets.ModelViewSet):
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
+    extra_data = serializers.SerializerMethodField()
+    phone_num_text = serializers.SerializerMethodField()
+
     class Meta:
         model = Employee
         fields = "__all__"
+
+    # def get_values(self, dictionary):
+    #     text_to_anonymize = "His name is Mr. Jones and his phone number is 212-555-5555"
+    #     # %%
+    #     analyzer = AnalyzerEngine()
+    #     analyzer_results = analyzer.analyze(text=text_to_anonymize, entities=["PHONE_NUMBER"], language='en')
+    #
+    #     print(analyzer_results)
+
+    def get_extra_data(self, obj):
+        anonymizer = AnonymizerEngine()
+
+        some_magic_key = "WmZq4t7w!z%C&F)J"
+
+        text = "My name is James Bond farzi admi"
+
+        anonymized_text = anonymizer.anonymize(
+            text=text,
+            analyzer_results=[
+                RecognizerResult(
+                    entity_type="PERSON",
+                    start=0,
+                    end=len(text),
+                    score=0.8,
+                ),
+            ],
+            operators={"PERSON": OperatorConfig("encrypt", {"key": some_magic_key})},
+        )
+
+        return anonymized_text.text
+
+    def get_phone_num_text(self, obj):
+        analyzer = AnalyzerEngine()
+        text = "His name is Mr. Jones and his phone number is 212-555-5555"
+        analyzer_results = analyzer.analyze(
+            text=text,
+            entities=["PHONE_NUMBER"],
+            language="en",
+        )
+
+        anonymizer = AnonymizerEngine()
+
+        anonymized_text = anonymizer.anonymize(
+            text=text,
+            analyzer_results=analyzer_results,
+        )
+
+        return anonymized_text.text
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
@@ -40,7 +89,7 @@ class EmployeeViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         print(self.request.tenant)
-        bigco = Client.objects.get(name='smallco')
+        bigco = Client.objects.get(name="bigco")
         connection.set_tenant(bigco)
         return Employee.objects.all()
 
